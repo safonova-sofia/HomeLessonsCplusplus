1854b848f8bcd0488117eee9dcf167916f6ee506
Safonova Sofia <sofia.safonova.aleksandrovna@gmail.com>
2024-02-23 01:18:05 +0300

    HW3

---
+++ HW3.cpp
@@ -0,0 +1,197 @@
+﻿#include <iostream>
+#include <chrono>
+#include <fstream>
+#include <random>
+
+std::random_device rd;
+std::mt19937 rng(rd());
+
+// иницилизируем как глобальные значения, чтобы не привести к переполнению стека
+const int kSize = 1000; // кол-во элементов массива
+long long array[kSize] = {};
+long long arrayQuickSort[kSize] = {};
+long long arrayBubbleSort[kSize] = {};
+long long arrayInsertionSort[kSize] = {};
+
+template <typename T>
+T GetRandomInRange(const T min, const T max)
+{
+    std::uniform_int_distribution<int> uid(min,max);
+    return uid(rng);
+}
+
+template <typename T, typename Arr>
+void FillArray(Arr * array, const T size, const T min, const T max) {
+    for (int i = 0; i < size; i++) {
+        array[i] = GetRandomInRange(min, max);
+    }
+}
+
+template <typename T, typename Arr>
+const void PrintArray(const Arr * array, const T size) {
+    for (int i = 0; i < size; i++)
+        std::cout << array[i] << " ";
+    std::cout << std::endl;
+}
+
+// 1. Quick Sort
+
+template <typename T, typename Arr>
+T Partition(Arr  * array, const T low, const T high) {
+
+    int i = low - 1;
+    int j = high + 1;
+    int pivot = array[low];
+
+    while (true) {
+        do {
+            i++;
+        } while (array[i] < pivot);
+
+        do {
+            j--;
+        } while (array[j] > pivot);
+
+        if (i >= j)
+            return j;
+
+        std::swap(array[i], array[j]);
+    }
+}
+
+template <typename T, typename Arr>
+T PartionRandom(Arr * array, const T low, const T high) {
+    int random = GetRandomInRange(low, high);
+    
+    std::swap(array[random], array[low]);
+
+    return Partition(array, low, high);
+}
+
+template <typename T, typename Arr>
+void QuickSort(Arr * array, const T low, const T high)
+{
+    if (low < high) {
+        int pivot = PartionRandom(array, low, high);
+
+        QuickSort(array, low, pivot);
+        QuickSort(array, pivot + 1, high);
+    }
+}
+
+// 2. Bubble Sort
+
+template <typename T, typename Arr>
+void BubbleSort(Arr * array, const T size) {
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+            if (array[j] > array[j + 1])
+            {
+                std::swap(array[j], array[j + 1]);
+            }
+    }
+}
+
+// 3. Insertion Sort
+
+template <typename T, typename Arr>
+void InsertionSort(Arr * array, const T size) {
+    for (int i = 1; i < size; i++)
+    {
+        int tmp = array[i];
+        int q = i - 1;
+
+        while ((q >= 0) && (array[q] > tmp))
+        {
+            array[q + 1] = array[q];
+            q--;
+        }
+        array[q + 1] = tmp;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    const int kMinValue = 0;
+    const int kMaxValue = 32767; // макс. значение rand(); используем для ф-ии с равномер. распределением для заполнения массива
+    
+    // Заполняем массив
+    FillArray(array, kSize, kMinValue, kMaxValue);
+
+    // Копии массива для сортировок
+    memcpy(arrayQuickSort, array, kSize * sizeof(long long));
+    memcpy(arrayBubbleSort, array, kSize * sizeof(long long));
+    memcpy(arrayInsertionSort, array, kSize * sizeof(long long));
+
+    // Кол-во элементов
+    std::cout << "Кол-во элементов: " << kSize << std::endl;
+
+    // Выводим неупорядоченный массив
+    std::cout << "Первоначальный массив" << std::endl;
+    PrintArray(array, kSize);
+
+    // Поток для записи результатов в файл
+    std::ofstream out("results.txt", std::ios::app);
+
+    // 1. Быстрая сортировка 
+    std::cout << std::endl << "1. Быстрая сортировка" << std::endl;
+
+    auto startTimeQuickSort = std::chrono::steady_clock::now();
+    QuickSort(arrayQuickSort, 0, kSize - 1);
+    auto endTimeQuickSort = std::chrono::steady_clock::now();
+
+    auto runtimeQuickSort = std::chrono::duration<double>(endTimeQuickSort - startTimeQuickSort).count();
+
+    // Быстрая сортировка. Отсортированный массив
+    std::cout << std::endl;
+    std::cout << "Быстрая сортировка. Остсортированный массив" << std::endl;
+    PrintArray(arrayQuickSort, kSize);
+
+    std::cout << std::endl;
+    std::cout << "Быстрая сортировка. RuntimeExecute: " << runtimeQuickSort << " sec" << std::endl;
+    out << runtimeQuickSort << std::endl;
+
+    // 2. Сортировка пузырьком
+    std::cout << std::endl << "2. Сортировка пузырьком" << std::endl;
+
+    auto startTimeBubbleSort = std::chrono::steady_clock::now();
+    BubbleSort(arrayBubbleSort, kSize);
+    auto endTimeBubbleSort = std::chrono::steady_clock::now();
+
+    auto runtimeBubbleSort = std::chrono::duration<double>(endTimeBubbleSort - startTimeBubbleSort).count();
+
+    // Сортировка пузырьком. Отсортированный массив
+    std::cout << std::endl;
+    std::cout << "Сортировка пузырьком. Остсортированный массив" << std::endl;
+    PrintArray(arrayBubbleSort, kSize);
+
+    std::cout << std::endl;
+    std::cout << "Сортировка пузырьком. RuntimeExecute: " << runtimeBubbleSort << " sec" << std::endl;
+    out << runtimeBubbleSort << std::endl;
+
+    // 3. Сортировка вставкой
+    std::cout << std::endl << "3. Сортировка вставкой" << std::endl;
+
+    auto startTimeInsertionSort = std::chrono::steady_clock::now();
+    InsertionSort(arrayInsertionSort, kSize);
+    auto endTimeInsertionSort = std::chrono::steady_clock::now();
+
+    auto runtimeInsertionSort = std::chrono::duration<double>(endTimeInsertionSort - startTimeInsertionSort).count();
+
+    // Сортировка вставкой. Отсортированный массив
+    std::cout << std::endl;
+    std::cout << "Сортировка вставкой. Остсортированный массив" << std::endl;
+    PrintArray(arrayInsertionSort, kSize);
+
+    std::cout << std::endl;
+    std::cout << "Сортировка вставкой. RuntimeExecute: " << runtimeInsertionSort << " sec" << std::endl;
+    out << runtimeInsertionSort << std::endl;
+
+    // Закрываем запись в файл
+    out.close();
+
+    return 0;
+}
---
+++ HomeWorks Archive/VW1.cpp
@@ -0,0 +1,154 @@
+﻿#include <iostream>
+#include <vector>
+
+// Вариант 2
+
+struct Account {
+	short id;
+	char login[16];
+	int password;
+};
+
+void toFullArray(int** array, int row, int column) {
+	srand(time(NULL));
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < column; j++) {
+			array[i][j] = 1 + rand() % 70;
+		}
+	}
+};
+
+void toSnowArray(int** array, int row, int column) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < column; j++) {
+			std::cout << array[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+};
+
+void sumAdditinalElements() {
+
+	int rowArray = 0;
+	int columnArray = 0;
+	int summa = 0;
+
+	std::cout << "Введите кол-во строк:" << std::endl;
+	std::cin >> rowArray;
+	std::cout << "Введите кол-во столбцов:" << std::endl;
+	std::cin >> columnArray;
+
+	int** array = new int* [rowArray];
+
+	//Создание массива
+	for (int i = 0; i < rowArray; i++) {
+		array[i] = new int[columnArray];
+	}
+
+	//Заполнение
+	toFullArray(array, rowArray, columnArray);
+
+	std::cout << std::endl;
+
+	//Вывод массива
+	toSnowArray(array, rowArray, columnArray);
+
+	// Сумма на побочных диагоналях при условии, что она квадратная
+	if (rowArray == columnArray) {
+		for (int i = 0; i < rowArray; i++) {
+			summa += array[i][rowArray - 1 - i];
+		}
+	}
+
+	std::cout << std::endl;
+	std::cout << "Сумма на побочных диагоналях матрицы: " << summa << std::endl;
+
+	// Возвращаем память
+	for (int i = 0; i < rowArray; i++) {
+		delete[] array[i];
+	}
+	delete[] array;
+};
+
+Account* createAccout(Account* Obj, const int length){
+	// Смотрим размер массива со структурами
+	// Если пустой - в создвем структуру
+	if (length == 0) {
+		Obj = new Account[length + 1];
+	}
+	// Если уже что-то есть - увеличиваем массив и переносим в "новый" массив
+	else {
+		Account* tmpObj = new Account[length + 1];
+		for (int i = 0; i < length; i++) {
+			tmpObj[i] = Obj[i];
+		}
+
+		delete[] Obj;
+
+		Obj = tmpObj;
+	}
+	return Obj;
+}
+
+auto deleteAccout() {
+
+}
+
+auto editAccout() {
+
+}
+
+auto printAccout() {
+
+}
+
+void controlAccount() {
+	bool flag = true; // продолжить ввод в цикле или остановить ввод
+	
+	int variation = 0; // выбор действия
+
+	Account* array = 0;
+	int length = 0; // кол-во акаунтов
+
+	while (flag != false) {
+		std::cout << "Выберите действие:" << std::endl;
+		// 1 - создать, 2 - удалить, 3 - редактировать, 4 - выводить
+		std::cin >> variation;
+		switch (variation)
+		{
+		case 1:
+			array = createAccout(array, length);
+			break;
+		case 2:
+			deleteAccout();
+			break;
+		case 3:
+			editAccout();
+			break;
+		case 4:
+			printAccout();
+			break;
+		default:
+			std::cout << "Ошибка ввода" << std::endl;
+			break;
+		} 
+		exit;
+	}
+
+	delete[] array;
+
+}
+
+
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	sumAdditinalElements();
+	std::cout << std::endl;
+
+	controlAccount();
+	std::cout << std::endl;
+
+	return 0;
+}
